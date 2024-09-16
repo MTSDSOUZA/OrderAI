8ce7251b5b6ec351bc6d851b09a15e306003f11d
MTSDSOUZA <matheus12ds@gmail.com>
2024-09-16 15:54:48 -0300

    mudança permição

--- src/main/java/com/example/orderAI/auth/SecurityConfig.java
+++ src/main/java/com/example/orderAI/auth/SecurityConfig.java
@@ -2,9 +2,7 @@ package com.example.orderAI.auth;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -17,14 +15,8 @@ public class SecurityConfig {
         http.csrf(csrf -> csrf.disable());
 
         // Autorização de requisições
-        http.authorizeHttpRequests(auth ->
-                auth
-                        .requestMatchers(HttpMethod.POST, "/login").permitAll()
-                        .requestMatchers(HttpMethod.GET, "/usuario").permitAll()
-                        .requestMatchers(HttpMethod.POST, "/usuario").permitAll()
-                        .requestMatchers("/h2-console/**").permitAll()
-                        .anyRequest().authenticated()
-        );
+        http.authorizeHttpRequests(auth -> auth
+                .anyRequest().permitAll());
 
         // Desabilitar a proteção de frame options para o H2 console
         http.headers(headers -> headers.frameOptions(frameOptions -> frameOptions.disable()));
@@ -40,4 +32,3 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 }
-
